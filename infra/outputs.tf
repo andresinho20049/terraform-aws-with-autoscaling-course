292f188ca4ce3fea609c9c8e8371e27d00ecd25f
Andresinho20049 <andre.andresinho2009@hotmail.com>
2025-06-26 01:02:26 -0300

    feat: Add Bastion Host module with necessary variables, outputs, and integration with VPC and EFS modules

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,8 @@ output "vpc_name" {
     description = "The name of the VPC."
     value       = module.main_vpc.vpc_name
 }
+
+output "bastion_instance_id" {
+    description = "The ID of the bastion host instance."
+    value       = var.create_bastion_host ? module.bastion_host[0].bastion_instance_id : null
+}
