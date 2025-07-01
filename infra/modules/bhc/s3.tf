826d39aed78b693b5970d03c45ea5f1d0aadcb39
Andresinho20049 <andre.andresinho2009@hotmail.com>
2025-06-30 23:25:20 -0300

    fix: Correct comment language for S3 bucket resource to improve clarity

--- infra/modules/bhc/s3.tf
+++ infra/modules/bhc/s3.tf
@@ -1,4 +1,4 @@
-# S3 bucket para arquivos temporários do bastion host
+# Bucket S3 for temporary files of the bastion host
 resource "aws_s3_bucket" "bhc_temp" {
   bucket = "${var.account_username}.${var.region}.s3.bhc-temp.${var.environment}"
   force_destroy = true
